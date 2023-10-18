c599f0ca55d508f49f30c30a87b41af3c92a87d6
Dalton Steele <dsteele92@gmail.com>
2023-10-18 17:16:50 +0200

    configuring cloudinary

--- src/components/sections/Intro/Intro.jsx
+++ src/components/sections/Intro/Intro.jsx
@@ -6,6 +6,13 @@ import { MainMenu } from 'components';
 export default function Intro() {
 	return (
 		<div className={Style.Intro}>
+			<div className={Style.BackgroundWrapper}>
+				<img
+					className={Style.Background}
+					src='https://res.cloudinary.com/dp1dn1ghw/image/upload/v1697638379/JD/mt_hood_unsplash_cjdqfv.jpg'
+					alt='mt hood background'
+				/>
+			</div>
 			<div className={Style.Tint}>
 				<div className={Style.Headers}>
 					<div className={Style.Title}>
