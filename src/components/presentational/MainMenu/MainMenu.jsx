17b6d4197cb57d418dac22d42a6dce2e88a1de2a
Dalton Steele <dsteele92@gmail.com>
2023-10-31 14:05:19 +0100

    building main menu and making responsive

--- src/components/presentational/MainMenu/MainMenu.jsx
+++ src/components/presentational/MainMenu/MainMenu.jsx
@@ -4,10 +4,18 @@ import Style from './mainMenu.module.scss';
 export default function MainMenu() {
 	return (
 		<menu className={Style.Menu}>
-			<section className={Style.About}></section>
-			<section className={Style.Reviews}></section>
-			<section className={Style.Forms}></section>
-			<section className={Style.Contact}></section>
+			<section className={Style.About}>
+				<h4>About Us</h4>
+			</section>
+			<section className={Style.Reviews}>
+				<h4>Reviews</h4>
+			</section>
+			<section className={Style.Forms}>
+				<h4>Patient Forms</h4>
+			</section>
+			<section className={Style.Contact}>
+				<h4>Contact Us</h4>
+			</section>
 		</menu>
 	);
 }
