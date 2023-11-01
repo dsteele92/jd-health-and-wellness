c1801c3abc43a2ba3b9e2db81832bea595647c89
Dalton Steele <dsteele92@gmail.com>
2023-11-01 16:55:26 +0100

    working on main menu bubble effects

--- src/components/presentational/MainMenu/MainMenu.jsx
+++ src/components/presentational/MainMenu/MainMenu.jsx
@@ -5,16 +5,41 @@ export default function MainMenu() {
 	return (
 		<menu className={Style.Menu}>
 			<section className={Style.About}>
-				<h4>About Us</h4>
+				<div className={Style.Bubble}>
+					<div className={Style.BubbleInner}></div>
+				</div>
+				<div className={Style.Content}>
+					<h4>About Us</h4>
+					<div className={Style.SubLinks}>
+						<h4>Our Services</h4>
+						<h4>Meet the Team</h4>
+						<h4>Community & Government</h4>
+					</div>
+				</div>
 			</section>
 			<section className={Style.Reviews}>
-				<h4>Reviews</h4>
+				<div className={Style.Bubble}>
+					<div className={Style.BubbleInner}></div>
+				</div>
+				<div className={Style.Content}>
+					<h4>Reviews</h4>
+				</div>
 			</section>
 			<section className={Style.Forms}>
-				<h4>Patient Forms</h4>
+				<div className={Style.Bubble}>
+					<div className={Style.BubbleInner}></div>
+				</div>
+				<div className={Style.Content}>
+					<h4>Patient Forms</h4>
+				</div>
 			</section>
 			<section className={Style.Contact}>
-				<h4>Contact Us</h4>
+				<div className={Style.Bubble}>
+					<div className={Style.BubbleInner}></div>
+				</div>
+				<div className={Style.Content}>
+					<h4>Contact Us</h4>
+				</div>
 			</section>
 		</menu>
 	);
