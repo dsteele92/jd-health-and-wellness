a17706d1fa7d1674570d8dd79884d60821f30e50
Dalton Steele <dsteele92@gmail.com>
2023-11-22 15:55:39 +0100

    added first CTA header section

--- src/components/sections/About/About.jsx
+++ src/components/sections/About/About.jsx
@@ -1,9 +1,29 @@
-import { React } from 'react';
+import { React, useEffect, useRef } from 'react';
 import Style from './about.module.scss';
 import { SectionHeader, useHasIntersected } from 'components';
 
 export default function About() {
 	const [aboutText, aboutTextIntersected] = useHasIntersected({ threshold: 0.25 });
+	const [CTA, CTAIntersected] = useHasIntersected({ threshold: 0.25 });
+
+	const service1name = useRef();
+
+	useEffect(() => {
+		// console.dir(service1name.current);
+		console.log(service1name.current.clientWidth);
+
+		const handleScroll = (event) => {
+			const windowHeight = window.innerHeight;
+			const currentScroll = window.pageYOffset;
+			console.log(windowHeight, currentScroll);
+		};
+
+		window.addEventListener('scroll', handleScroll);
+
+		return () => {
+			window.removeEventListener('scroll', handleScroll);
+		};
+	}, []);
 
 	return (
 		<div className={Style.About}>
@@ -25,7 +45,7 @@ export default function About() {
 			</div>
 			<div className={Style.Services}>
 				<div className={Style.Service}>
-					<div className={Style.ServiceName}>
+					<div className={Style.ServName} ref={service1name}>
 						<img
 							src='https://res.cloudinary.com/dp1dn1ghw/image/upload/v1697654802/JD/icons8-family-96_vuffga.png'
 							alt='family medicine icon'
@@ -33,42 +53,51 @@ export default function About() {
 						/>
 						<div className={Style.Text}>Family Medicine</div>
 					</div>
-					<div className={Style.ServiceDescription}></div>
+					<div className={Style.ServDescription}></div>
 				</div>
 				<div className={Style.Service}>
-					<div className={Style.ServiceName}>
+					<div className={Style.ServName}>
 						<div className={Style.Icon}></div>
 						<div className={Style.Text}></div>
 					</div>
-					<div className={Style.ServiceDescription}></div>
+					<div className={Style.ServDescription}></div>
 				</div>
 				<div className={Style.Service}>
-					<div className={Style.ServiceName}>
+					<div className={Style.ServName}>
 						<div className={Style.Icon}></div>
 						<div className={Style.Text}></div>
 					</div>
-					<div className={Style.ServiceDescription}></div>
+					<div className={Style.ServDescription}></div>
 				</div>
 				<div className={Style.Service}>
-					<div className={Style.ServiceName}>
+					<div className={Style.ServName}>
 						<div className={Style.Icon}></div>
 						<div className={Style.Text}></div>
 					</div>
-					<div className={Style.ServiceDescription}></div>
+					<div className={Style.ServDescription}></div>
 				</div>
 				<div className={Style.Service}>
-					<div className={Style.ServiceName}>
+					<div className={Style.ServName}>
 						<div className={Style.Icon}></div>
 						<div className={Style.Text}></div>
 					</div>
-					<div className={Style.ServiceDescription}></div>
+					<div className={Style.ServDescription}></div>
 				</div>
 				<div className={Style.Service}>
-					<div className={Style.ServiceName}>
+					<div className={Style.ServName}>
 						<div className={Style.Icon}></div>
 						<div className={Style.Text}></div>
 					</div>
-					<div className={Style.ServiceDescription}></div>
+					<div className={Style.ServDescription}></div>
+				</div>
+			</div>
+			<div className={CTAIntersected ? Style.CTAShow : Style.CTA} ref={CTA}>
+				<div className={Style.Bubble}>
+					<div className={Style.BubbleInner}></div>
+				</div>
+				<div className={Style.Content}>
+					<h1>Schedule an appointment today!</h1>
+					<h2>Call (503) 877-1995</h2>
 				</div>
 			</div>
 		</div>
