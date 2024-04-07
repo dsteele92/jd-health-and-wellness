d4a883c56e6676aab647fe908720c6d3fd4b5ee9
Dalton Steele <dsteele92@gmail.com>
2024-04-07 18:17:19 +0200

    cleaned up unused components and variables and made intro page responsive

--- src/components/sections/Intro/Intro.jsx
+++ src/components/sections/Intro/Intro.jsx
@@ -8,25 +8,27 @@ const Intro = forwardRef((props, ref) => {
 	return (
 		<div className={Style.Intro} ref={ref}>
 			<div className={Style.Tint}>
-				<div className={Style.Headers}>
-					<div className={Style.Title}>
-						<div className={Style.JD}>JD</div>
-						<div className={Style.HW}>
-							Health &<br />
-							Wellness
+				<div className={Style.IntroContent}>
+					<div className={Style.Headers}>
+						<div className={Style.Title}>
+							<div className={Style.JD}>JD</div>
+							<div className={Style.HW}>
+								Health &<br />
+								Wellness
+							</div>
 						</div>
 					</div>
-				</div>
-				<div className={Style.Center}>
-					<h2 className={Style.Subtitle}>Serving Oregon for over 40 years</h2>
-					<div className={Style.Buttons}>
-						<ButtonSquare text='Patient Forms' minWidth='16rem' onClick={scrollToForms} />
-						<a
-							href='https://patientportal.intelichart.com/login/Account/Login?ReturnUrl=%2f'
-							target='_blank'
-							rel='noopener noreferrer'>
-							<ButtonSquare text='Patient Portal' minWidth='16rem' />
-						</a>
+					<div className={Style.Center}>
+						<h2 className={Style.Subtitle}>Serving Oregon for over 40 years</h2>
+						<div className={Style.Buttons}>
+							<ButtonSquare text='Patient Forms' minWidth='14rem' onClick={scrollToForms} />
+							<a
+								href='https://patientportal.intelichart.com/login/Account/Login?ReturnUrl=%2f'
+								target='_blank'
+								rel='noopener noreferrer'>
+								<ButtonSquare text='Patient Portal' minWidth='14rem' />
+							</a>
+						</div>
 					</div>
 				</div>
 				<div className={Style.Arrow}>
