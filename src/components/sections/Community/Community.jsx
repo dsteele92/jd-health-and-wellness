7292c9ff5c9e5fcc1f961911569de810c399cca1
Dalton Steele <dsteele92@gmail.com>
2024-04-29 01:16:09 +0200

    added modal for community links

--- src/components/sections/Community/Community.jsx
+++ src/components/sections/Community/Community.jsx
@@ -1,27 +1,54 @@
-import { React, forwardRef } from 'react';
+import { React, forwardRef, useMemo, useState } from 'react';
 import Style from './community.module.scss';
 import { useHasIntersected, SectionHeader } from 'components';
 import { communityLinks } from 'content';
-// import { Modal } from '@mantine/core';
-// import { useDisclosure } from '@mantine/hooks';
+import { Modal } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IoMdCloseCircle } from 'react-icons/io';
 
 const Community = forwardRef((props, ref) => {
+	const [opened, { open, close }] = useDisclosure(false);
 	const [community, communityIntersected] = useHasIntersected({ threshold: 0.25 });
-	// const [articleSelected, setArticleSelected] = useState(-1);
-	// const [opened, { open, close }] = useDisclosure(false);
+	const [selected, setSelected] = useState(0);
 
-	// const formModal = useMemo(() => {
-	// 	return <IntakeForm />;
-	// }, []);
+	const modal = useMemo(() => {
+		return (
+			<div className={Style.ModalContent}>
+				<div className={Style.ModalHeader}>
+					<div className={Style.ModalClose} onClick={close}>
+						<IoMdCloseCircle />
+					</div>
+					<h2 className={Style.ModalTitle}>{communityLinks[selected].name}</h2>
+				</div>
+				{communityLinks[selected].links.map((link, index) => (
+					<iframe key={index} title='PDF Viewer' src={link} width='100%' height='800px' />
+				))}
+				{communityLinks[selected].image && (
+					<img
+						src={communityLinks[selected].image}
+						alt={communityLinks[selected].altText}
+						className={Style.ModalImage}
+					/>
+				)}
+			</div>
+		);
+	}, [selected, close]);
 
 	return (
 		<div className={Style.Community} ref={ref}>
+			<Modal
+				opened={opened}
+				onClose={close}
+				size='100%'
+				classNames={{ body: Style.Modal }}
+				withCloseButton={false}>
+				{modal}
+			</Modal>
 			<div className={Style.Tint}></div>
 			<div className={Style.InnerWrapper}>
 				<section className={Style.Stripe}>
 					<SectionHeader top='Community' />
 					<div className={Style.StripeContent}>
-						{/* <h2>Say something</h2> */}
 						<p>
 							We named the Clinic JD after the children of Both Felicity and Eric Davis Josiah, Jazmyn,
 							and Lastly Joshua. The name JD was inspired by Joshua Davis the youngest grandchild of Dr.
@@ -39,7 +66,13 @@ const Community = forwardRef((props, ref) => {
 				<div className={Style.Right} ref={community}>
 					<div className={Style[`RightContent${communityIntersected ? 'Show' : ''}`]}>
 						{communityLinks.map((article, index) => (
-							<div key={index} className={Style[`Article${index}`]}>
+							<div
+								key={index}
+								className={Style[`Article${index}`]}
+								onClick={() => {
+									open();
+									setSelected(index);
+								}}>
 								<h1 className={Style.Number}>{`0${index + 1}`}</h1>
 								<h3 className={Style.Name}>{article.name}</h3>
 							</div>
--- src/content/community.js
+++ src/content/community.js
@@ -1,18 +1,25 @@
 export const communityLinks = [
 	{
 		name: 'Union Gospel Misson of Salem Partners with Local Clinic to Provide Telehealth Services to Guests',
-		link: '',
+		links: ['https://jd-health-and-wellness.s3.us-west-2.amazonaws.com/UGM_telehealth_news_release.pdf'],
 	},
 	{
 		name: 'Mental Health, Medical Care Now Part of Life at Simonka Place',
-		link: '',
+		links: [
+			'https://jd-health-and-wellness.s3.us-west-2.amazonaws.com/Mental_health__medical_care_now_part_of_life_at_Simonka_Place_-_Salem_Reporter_(1).pdf',
+		],
 	},
 	{
 		name: 'Alternative Medicine Finds Path to CCOs',
-		link: '',
+		links: ['https://jd-health-and-wellness.s3.us-west-2.amazonaws.com/lund_report.pdf'],
 	},
 	{
 		name: 'Eric Davis Receives Letters from President Obama',
-		link: '',
+		links: [
+			'https://jd-health-and-wellness.s3.us-west-2.amazonaws.com/Letter_from_OBAMA_7-1-2016.pdf',
+			'https://jd-health-and-wellness.s3.us-west-2.amazonaws.com/Obama_Second_Letter.pdf',
+		],
+		image: 'https://jd-health-and-wellness.s3.us-west-2.amazonaws.com/biden.jpeg',
+		altText: 'Eric Davis with President Biden',
 	},
 ];
